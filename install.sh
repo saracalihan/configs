b8f251a5fe21a5448cf721fda2c4e8cf6262f927
saracalihan <saracalihan@gmail.com>
2021-05-20 15:38:04 +0300

    .

--- install.sh
+++ install.sh
@@ -1,59 +1,55 @@
 pmInstallCommand="sudo apt install"
 here=$PWD
-green=`tput setaf 2`
-reset=`tput sgr0`
 
-cd
+
 #install git
-echo "+++++++++++++| ${red}INSTALL GIT${reset} |+++++++++++++"
+#echo "+++++++++++++| INSTALL GIT |+++++++++++++"
 #$pmInstallCommand git
 
-cd
-
-#install vim
-echo "+++++++++++++| ${red}INSTALL VIM${reset} |+++++++++++++"
+# install vim
+echo "+++++++++++++| "INSTALL VIM |+++++++++++++"
 $pmInstallCommand vim
 cp $here/.vimrc /home/saracalihan/.vimrc
-echo "+++++++++++++| ${red}INSTALL VUNDLE${reset} |+++++++++++++"
+echo "+++++++++++++| INSTALL VUNDLE |+++++++++++++"
 git clone https://github.com/VundleVim/Vundle.vim.git /home/saracalihan/.vim/bundle/Vundle.vim
 git clone https://github.com/danilo-augusto/vim-afterglow.git /home/saracalihan/.vim/bundle/afterglow
-echo "+++++++++++++| ${red}SET .VIMRC${reset} |+++++++++++++"
+echo "+++++++++++++| SET .VIMRC |+++++++++++++"
 source /home/saracalihan/.vimrc
-echo "+++++++++++++| ${red}INSTALL VIM PLUGINS${reset} |+++++++++++++"
+echo "+++++++++++++| INSTALL VIM PLUGINS |+++++++++++++"
 vim +PluginInstall +qall
 cd
 
-#install node & npm
-echo "+++++++++++++| ${red}INSTALL NODE & NPM${reset} |+++++++++++++"
+# install node & npm
+echo "+++++++++++++| INSTALL NODE & NPM |+++++++++++++"
 $pmInstallCommand nodejs npm
 node --version
 npm --version
 
-echo "+++++++++++++| ${red}INSTALL YARN${reset} |+++++++++++++"
+echo "+++++++++++++| INSTALL YARN |+++++++++++++"
 npm install -g yarn
 yarn --version
 
 cd
 
 #get all projects
-echo "+++++++++++++| ${red}GET ALL PROJECTS${reset} |+++++++++++++"
+echo "+++++++++++++| GET ALL PROJECTS |+++++++++++++"
 $here/./install.sh
 cd
 
 8 #get all projects                                                                                        
-echo "+++++++++++++| ${red}INSTALL BAT${reset} |+++++++++++++"
+echo "+++++++++++++| INSTALL BAT |+++++++++++++"
 $here/./install.sh
 cd  
 
-#install zsh
-#echo "+++++++++++++| ${red}INSTALL ZSH${reset} |+++++++++++++"
-#$pmInstallCommand zsh
+# install zsh
+echo "+++++++++++++| INSTALL ZSH |+++++++++++++"
+$pmInstallCommand zsh
 
-#echo "+++++++++++++| ${red}INSTALL OH-MY-ZSH${reset} |+++++++++++++"
-#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#cp $here/.zshrc /home/saracalihan/.zshrc
-#source /home/saracalihan/.zshrc
+echo "+++++++++++++| INSTALL OH-MY-ZSH |+++++++++++++"
+cp $here/.zshrc /home/saracalihan/.zshrc
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source /home/saracalihan/.zshrc
 
 
-#echo "+++++++++++++| ${red}CHANGE DEFAULT SHELL TO ZSH${reset} |+++++++++++++"
+#echo "+++++++++++++| CHANGE DEFAULT SHELL TO ZSH |+++++++++++++"
 #sudo chsh -s $(which zsh)
